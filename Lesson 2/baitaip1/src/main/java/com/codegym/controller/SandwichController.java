03527da4785e6bf74b40accef529178582ee87e1
= <=>
2021-10-12 17:22:50 +0700

    MD4 bai 2

---
+++ Lesson 2/baitaip1/src/main/java/com/codegym/controller/SandwichController.java
@@ -0,0 +1,25 @@
+package com.codegym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SandwichController {
+    @GetMapping("home")
+    public String home() {
+        return "index";
+    }
+    @GetMapping("/save")
+    public String save(@RequestParam("condiment") String[] condiment, Model model) {
+        String spices = "";
+        for(String s : condiment) {
+            spices += s+ ", ";
+        }
+        model.addAttribute("spices",spices);
+        return "index";
+    }
+}
